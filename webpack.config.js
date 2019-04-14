a25d7bda8681da8c404cd6e38ed32e28815986bd
toto011 <vukmilicevic@mail.ru>
2019-04-14 09:57:33 +0200

    1st

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+module.exports = {
+  entry: path.join(__dirname,"src","index.js"),
+  output: {
+    path: path.join(__dirname,"build"),
+    filename: "index.bundle.js",
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"), "node_modules",
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname,"src"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          "eslint-loader",
+        ],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: [
+          "file-loader",
+        ],
+      },
+    ],
+  },  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,"src","index.html"),
+    }),
+  ],
+};
